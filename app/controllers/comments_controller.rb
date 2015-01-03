e932316670d5df511550b45e6c6c90a4064ba320
Busan Coders <busancoders@gmail.com>
2015-01-03 21:36:03 +0000

    Completed Comments. Fixed issue with routes for resources :comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,46 +1,40 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
-  respond_to :html
-
-  def index
-    @comments = Comment.all
-    respond_with(@comments)
-  end
-
-  def show
-    respond_with(@comment)
-  end
-
-  def new
-    @comment = Comment.new
-    respond_with(@comment)
-  end
-
-  def edit
-  end
+  before_filter :authenticate_user!
 
   def create
-    @comment = Comment.new(comment_params)
-    @comment.save
-    respond_with(@comment)
+    @link = Link.find(params[:link_id])
+    @comment = @link.comments.new(comment_params)
+    @comment.user = current_user
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @link, notice: 'Comment was successfully created.' }
+        format.json { render json: @comment, status: :created, location: @comment }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
-  def update
-    @comment.update(comment_params)
-    respond_with(@comment)
-  end
-
+  # DELETE /comments/1
+  # DELETE /comments/1.json
   def destroy
     @comment.destroy
-    respond_with(@comment)
+    respond_to do |format|
+      format.html { redirect_to :back, notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
     def set_comment
       @comment = Comment.find(params[:id])
     end
 
+    # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
       params.require(:comment).permit(:link_id, :body, :user_id)
     end
